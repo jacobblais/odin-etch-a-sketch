09a0b357716aaafe915a17f19b6e71e2a2e8d37e
Jacob Blais <180109408+jacobblais@users.noreply.github.com>
2025-12-04 17:12:38 -0500

    Added pixel colour change on hover

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const container = document.getElementById("container");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.style = "display: flex; flex-grow: 1;";
-    container.appendChild(row);
+function createRow() {
+    let sidelen = 0;
+    while (sidelen <= 0 || sidelen > 100) {
+        sidelen = prompt("What is the width of the square?");
+    }
 
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.style = "border: 1px solid black; background-color: white; flex-grow: 1;";
-        row.appendChild(div);
+    container.innerHTML = "";
+
+    for (let i = 0; i < sidelen; i++) {
+        const row = document.createElement("div");
+        row.style = "display: flex; flex-grow: 1;";
+        container.appendChild(row);
+
+        for (let j = 0; j < sidelen; j++) {
+            const div = document.createElement("div");
+            div.style = "border: 1px solid black; background-color: white; flex-grow: 1;";
+            row.appendChild(div);
+
+            div.addEventListener("mouseover", () => div.style.backgroundColor = "black");
+        }
     }
 }
+
+const refresh = document.getElementById("refresh");
+refresh.addEventListener("click", createRow);
