da2b4967f6ea1a53da7e873bed63c36620945db7
Jacob Blais <180109408+jacobblais@users.noreply.github.com>
2025-12-04 22:47:22 -0500

    Completed project, edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-etch-a-sketch
+
+This project will showcase Javascript working with the HTML and CSS by both creating HTML elements using user input and changing these elements' CSS properties.
+
+The project is a simple pixel art tool where you can draw by hovering over pixels. Each pixel hovered over will change to a random colour, and gain opacity. Ten passes over one pixel makes it completely opaque.
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("container");
+const colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"];
 
 function createRow() {
     let sidelen = 0;
@@ -15,10 +16,14 @@ function createRow() {
 
         for (let j = 0; j < sidelen; j++) {
             const div = document.createElement("div");
-            div.style = "border: 1px solid black; background-color: white; flex-grow: 1;";
+            div.style = "border: 1px solid black; background-color: white; flex-grow: 1; opacity: 0;";
             row.appendChild(div);
 
-            div.addEventListener("mouseover", () => div.style.backgroundColor = "black");
+            div.addEventListener("mouseover", () => {
+                let index = Math.floor(Math.random() * colors.length);
+                div.style.backgroundColor = colors[index];
+                div.style.opacity = parseFloat(getComputedStyle(div).opacity) + 0.1;
+            });
         }
     }
 }
